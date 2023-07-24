adcc4f7b65629fa57057cbc71ee6ed73ba56d72f
Stephan Lorbek <stephan.lorbek@uni-graz.at>
2023-07-24 00:02:53 -0700

    fixed prefilled moodle date selection boxes

--- amd/src/externallib.js
+++ amd/src/externallib.js
@@ -2,8 +2,18 @@ import jQuery from 'jquery';
 
 export const clearSelection = () => {
     jQuery(document).ready(function() {
-        jQuery('#fitem_id_profile_field_gebdat .custom-select').each(function() {
-            jQuery(this).val('');
+        jQuery(".custom-select[name*=\"profile_field_gebdat\"]").append(new Option(" ", "-1"));
+        jQuery(".custom-select[name*=\"profile_field_gebdat\"]").val("-1");
+
+        jQuery('form').on('submit', function(event) {
+            $('select[name*="profile_field_gebdat"]').each(function() {
+                if ($(this).find('option:selected[value="-1"]').length > 0) {
+                    event.preventDefault();
+                    alert("Bitte geben Sie Ihr Geburtsdatum an");
+                    return false;
+                }
+            });
         });
     });
 };
+
